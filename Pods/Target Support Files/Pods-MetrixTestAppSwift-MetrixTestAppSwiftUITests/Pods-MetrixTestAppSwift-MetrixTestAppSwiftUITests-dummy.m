d70f635598dad57dfee267e74e4ce20b5e70427a
Matin <m.zareimatin@gmail.com>
2022-02-20 12:42:14 +0330

    chore: remove Pods directory

--- Pods/Target Support Files/Pods-MetrixTestAppSwift-MetrixTestAppSwiftUITests/Pods-MetrixTestAppSwift-MetrixTestAppSwiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MetrixTestAppSwift_MetrixTestAppSwiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MetrixTestAppSwift_MetrixTestAppSwiftUITests
-@end
